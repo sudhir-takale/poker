4debe8046716af35104fd8a5df8349d2406dec7f
Sudhir Takale <sudhirtakale99@gmail.com>
2024-05-10 16:55:25 +0530

    added code to evaluate pair

--- src/main/java/com/amaap/poker/domain/service/handrank/PairHandler.java
+++ src/main/java/com/amaap/poker/domain/service/handrank/PairHandler.java
@@ -1,7 +1,12 @@
 package com.amaap.poker.domain.service.handrank;
 
+import com.amaap.poker.domain.model.entity.Card;
 import com.amaap.poker.domain.model.entity.Hand;
 import com.amaap.poker.domain.service.HandRankHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PairHandler implements HandRankHandler {
     private HandRankHandler successor;
@@ -14,6 +19,20 @@ public class PairHandler implements HandRankHandler {
 
     @Override
     public String evaluateHand(Hand hand) {
-        return "";
+        List<Card> cards = hand.getCards();
+
+        Map<String, Integer> valueCounts = new HashMap<>();
+        for (Card card : cards) {
+            String value = card.getValue();
+            valueCounts.put(value, valueCounts.getOrDefault(value, 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : valueCounts.entrySet()) {
+            if (entry.getValue() == 2) {
+                return "Pair";
+            }
+        }
+
+        return successor.evaluateHand(hand);
     }
 }
