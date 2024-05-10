9fe76028d93285092c2c4c51792ffa6efb997662
Sudhir Takale <sudhirtakale99@gmail.com>
2024-05-10 17:07:09 +0530

    added code to evaluate fourofkind card

--- src/main/java/com/amaap/poker/domain/service/handrank/StraightFlushHandler.java
+++ src/main/java/com/amaap/poker/domain/service/handrank/StraightFlushHandler.java
@@ -1,7 +1,11 @@
 package com.amaap.poker.domain.service.handrank;
 
+import com.amaap.poker.domain.model.entity.Card;
 import com.amaap.poker.domain.model.entity.Hand;
 import com.amaap.poker.domain.service.HandRankHandler;
+
+import java.util.Collections;
+import java.util.List;
 
 public class StraightFlushHandler implements HandRankHandler {
     private HandRankHandler successor;
@@ -15,7 +19,37 @@ public class StraightFlushHandler implements HandRankHandler {
     @Override
     public String evaluateHand(Hand hand) {
 
+        List<Card> cards = hand.getCards();
 
-        return "";
+        Collections.sort(cards, (c1, c2) -> getValueRank(c1.getValue()) - getValueRank(c2.getValue()));
+
+        boolean isConsecutive = true;
+        for (int i = 0; i < cards.size() - 1; i++) {
+            if (getValueRank(cards.get(i + 1).getValue()) - getValueRank(cards.get(i).getValue()) != 1) {
+                isConsecutive = false;
+                break;
+            }
+        }
+
+        String suit = cards.get(0).getSuit();
+        boolean isSameSuit = cards.stream().allMatch(card -> card.getSuit().equals(suit));
+
+        if (isConsecutive && isSameSuit) {
+            return "Straight Flush";
+        } else {
+            return successor.evaluateHand(hand);
+        }
     }
+
+    private int getValueRank(String value) {
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].equals(value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
