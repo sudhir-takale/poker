efae596017bb835139b16af3d9c41805681d1a12
Sudhir Takale <sudhirtakale99@gmail.com>
2024-05-10 16:59:11 +0530

    added code to check fo straight

--- src/main/java/com/amaap/poker/domain/service/handrank/StraightHandler.java
+++ src/main/java/com/amaap/poker/domain/service/handrank/StraightHandler.java
@@ -1,7 +1,13 @@
 package com.amaap.poker.domain.service.handrank;
 
+import com.amaap.poker.domain.model.entity.Card;
 import com.amaap.poker.domain.model.entity.Hand;
 import com.amaap.poker.domain.service.HandRankHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class StraightHandler implements HandRankHandler {
     private HandRankHandler successor;
@@ -14,6 +20,43 @@ public class StraightHandler implements HandRankHandler {
 
     @Override
     public String evaluateHand(Hand hand) {
-        return "";
+
+        List<Card> cards = hand.getCards();
+
+        Collections.sort(cards, (c1, c2) -> getValueRank(c1.getValue()) - getValueRank(c2.getValue()));
+
+        Set<Integer> rankSet = new HashSet<>();
+        for (Card card : cards) {
+            rankSet.add(getValueRank(card.getValue()));
+        }
+
+        if (rankSet.size() == 5) {
+            boolean isConsecutive = true;
+            for (int i = 0; i < cards.size() - 1; i++) {
+                if (getValueRank(cards.get(i + 1).getValue()) - getValueRank(cards.get(i).getValue()) != 1) {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+            if (isConsecutive) {
+                return "Straight";
+            }
+        }
+        return successor.evaluateHand(hand);
     }
+
+
+    private int getValueRank(String value) {
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].equals(value)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
+
+
